79f7b2237328efd00974326c20399a296f196c3f
Andrew Rothstein <andrew.rothstein@gmail.com>
2014-11-21 14:56:36 -0500

    fix typos

--- boot.sh
+++ boot.sh
@@ -7,7 +7,7 @@ make discovery-url
 vagrant up
 eval `ssh-agent -s`
 ssh-add ~/.vagrant.d/insecure_private_key
-ssh-add $DEIS_PLATFORM_SSH_PRIVATEKEY
+ssh-add $DEIS_PLATFORM_SSH_PRIVATE_KEY
 ssh-add -l
 deisctl config platform set sshPrivateKey=$DEIS_PLATFORM_SSH_PRIVATE_KEY
 deisctl config platform set domain=$DEIS_DOMAIN
@@ -17,4 +17,4 @@ deisctl list
 
 # example for registering a user
 deis register http://deis.local3.deisapp.com --username="$DEIS_SUPERUSER_NAME" --password="$DEIS_SUPERUSER_PASSWD" --email="$DEIS_SUPERUSER_EMAIL"
-deis keys:add $DEIS_SUPERUSER_SSHPUBLIC_KEY
+deis keys:add $DEIS_SUPERUSER_SSH_PUBLIC_KEY
