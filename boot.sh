c217088be19d55a8f8736d197ca4b6413d0a0d6d
Andrew Rothstein <andrew.rothstein@gmail.com>
2014-11-21 00:05:07 -0500

    added bootstrap script

--- boot.sh
+++ boot.sh
@@ -4,11 +4,13 @@ make discovery-url
 vagrant up
 eval `ssh-agent -s`
 ssh-add ~/.vagrant.d/insecure_private_key
-ssh-add ~/.ssh/deis
+DEIS_PLATFORM_SSHPRIVATEKEY="~/.ssh/deis"
+ssh-add $DEIS_PLATFORM_SSHPRIVATEKEY
 ssh-add -l
-export DEISCTL_TUNNEL=deis.local3.deisapp.com
-deisctl config platform set sshPrivateKey=~/.ssh/deis
-deisctl config platform set domain=local3.deisapp.com
+export DEIS_DOMAIN="local3.deisapp.com"
+export DEISCTL_TUNNEL=deis.$DEIS_DOMAIN
+deisctl config platform set sshPrivateKey=$DEIS_PLATFORM_SSHPRIVATE_KEY
+deisctl config platform set domain=$DEIS_DOMAIN
 deisctl install platform
 deisctl start platform
 deisctl list
@@ -17,7 +19,6 @@ deisctl list
 DEIS_SUPERUSER_NAME="drew"
 DEIS_SUPERUSER_PASSWD="deeznuts"
 DEIS_SUPERUSER_EMAIL="drew@blackhole.com"
+DEIS_SUPERUSER_SSHPUBLIC_KEY="~/.ssh/id_rsa.pub"
 deis register http://deis.local3.deisapp.com --username="$DEIS_SUPERUSER_NAME" --password="$DEIS_SUPERUSER_PASSWD" --email="$DEIS_SUPERUSER_EMAIL"
-
-
-
+deis keys:add $DEISSUPERUSER_SSHPUBLIC_KEY
