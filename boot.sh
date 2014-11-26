b23a7bf91b4705897d34483b4cc0572673441882
Andrew Rothstein <andrew.rothstein@gmail.com>
2014-11-25 21:43:09 -0500

    fix indentation

--- boot.sh
+++ boot.sh
@@ -12,5 +12,9 @@ deisctl start platform
 deisctl list
 
 # example for registering a user
-deis register http://deis.local3.deisapp.com --username="$DEIS_SUPERUSER_NAME" --password="$DEIS_SUPERUSER_PASSWD" --email="$DEIS_SUPERUSER_EMAIL"
+deis register http://deis.local3.deisapp.com \
+     --username="$DEIS_SUPERUSER_NAME" \
+     --password="$DEIS_SUPERUSER_PASSWD" \
+     --email="$DEIS_SUPERUSER_EMAIL"
+
 deis keys:add $DEIS_SUPERUSER_SSH_PUBLIC_KEY
