7c6787643efe971541abeb7b3d384ce9438ea14f
Andrew Rothstein <andrew.rothstein@gmail.com>
2014-11-29 22:44:53 -0500

    deisctl config set seems to require some time to reach consensus

--- boot.sh
+++ boot.sh
@@ -4,7 +4,9 @@ set -x # echo on
 make discovery-url
 vagrant up
 deisctl config platform set sshPrivateKey=$DEIS_PLATFORM_SSH_PRIVATE_KEY
+sleep 1
 deisctl config platform set domain=$DEIS_DOMAIN
+sleep 1
 deisctl install platform
 deisctl start platform
 deisctl list
